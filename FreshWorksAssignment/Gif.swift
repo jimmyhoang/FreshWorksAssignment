b4d543958492f126d2ef41d9e9852a7056e88a2a
Jimmy Hoang <hoangjimmy94@gmail.com>
2017-10-10 15:11:52 -0700

    Initial Gif class

---
+++ FreshWorksAssignment/Gif.swift
@@ -0,0 +1,16 @@
+//
+//  Gif.swift
+//  FreshWorksAssignment
+//
+//  Created by Jimmy Hoang on 2017-10-10.
+//  Copyright © 2017 Jimmy Hoang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Gif {
+    
+    var gifImage: UIImage = UIImage()
+    var gifURL: String?
+}
