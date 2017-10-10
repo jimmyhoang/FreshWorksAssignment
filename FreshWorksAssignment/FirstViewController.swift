cd0d1b9581eace18e4ef0a427d9d0ac11b3d8f87
Jimmy Hoang <hoangjimmy94@gmail.com>
2017-10-10 14:59:06 -0700

    Initial commit

---
+++ FreshWorksAssignment/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FreshWorksAssignment
+//
+//  Created by Jimmy Hoang on 2017-10-10.
+//  Copyright © 2017 Jimmy Hoang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
